5895f22c3ff4cea8ac5d8c23483def338707050d
Carlos Fernández <fernandez.isc@gmail.com>
2016-10-18 19:58:28 -0500

    Creación de servidor plano monolitico

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ var server = http.createServer(function(req,res){
     res.writeHead(
         200,
         {
-            'Content-Type':
+            'Content-Type':'text/plain',
+            'Server':'Buho@0.0.0'
         }
     );
+    res.write('Hola desde el server...');
+    res.end();
+});
+// Poniendo a escuchar al server
+server.listen(3000,'127.0.0.1',function(){
+    console.log("> Server escuchando en " + "http://127.0.0.1:3000/ ...");
 });
