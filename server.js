02be629722fcc0651f2e4fc99a4165cdc5c03231
Carlos Fernández <fernandez.isc@gmail.com>
2016-10-19 18:24:44 -0500

    Change in server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// Cargando el modulo http
+// cargando modulo http
 var http = require('http');
-// Cargar las libreria colors
+//Cargar la libreria colores
 var colors = require('colors');
-// Generando un tema
+//Generando un tema
 colors.setTheme({
   silly: 'rainbow',
   input: 'grey',
@@ -15,24 +15,25 @@ colors.setTheme({
   debug: 'blue',
   error: 'red'
 });
+//obteniendo configuraciones
+var config = require("./config/config");
 
-// Obteniendo configuraciones
-var IP = '127.0.0.1';
-var PORT = 3000;
-
-//Crando el server
+//console.log(`> contenido del modulo= ${modulo}`)
+var IP = config.IP;
+var PORT = config.PORT;
+// Creando el server
 var server = http.createServer(function(req,res){
     res.writeHead(
-        200,
-        {
+        200,{
             'Content-Type':'text/plain',
-            'Server':'Buho@0.0.0'
+            'server':'Buho@0.0.0'
         }
     );
-    res.write('Hola desde el server...');
-    res.end();
+
+res.write('Hola desde el server...');
+res.end();
 });
 // Poniendo a escuchar al server
-server.listen(PORT,IP,function(){
-    console.log("> Server escuchando en ".info + `http://${IP}:${PORT}/ ...`.info);
+server.listen(PORT, IP, function(){ 
+    console.log("> server escuchando en ".info+`http://${IP}:${PORT}/ ...`.info);
 });
