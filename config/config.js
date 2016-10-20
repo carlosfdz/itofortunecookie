13968b6347fd5f704ea385de2e0e3bc553d224e7
Carlos Fernandez <fernandez.isc@gmail.com>
2016-10-20 18:25:31 -0500

    Add Path

--- config/config.js
+++ config/config.js
@@ -4,5 +4,6 @@
 //exports.numero1 = 11;
 module.exports = {
     'IP' : process.env.IP || '127.0.0.1',
-    'PORT' : process.env.PORT || 3000
+    'PORT' : process.env.PORT || 3000,
+    'STATIC_PATH': './static' 
 };
