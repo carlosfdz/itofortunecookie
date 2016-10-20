0e0f7c7c56fb295d806a8c0d5311628490311265
Carlos Fernandez <fernandez.isc@gmail.com>
2016-10-20 15:15:42 -0500

    Cambios en en colors

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-// cargando modulo http
+// Cargando el modulo http
 var http = require('http');
-//Cargar la libreria colores
+// Cargando la libreria colors
 var colors = require('colors');
-//Generando un tema
+var fs = require('fs');
+// Generando un tema
 colors.setTheme({
   silly: 'rainbow',
   input: 'grey',
@@ -11,29 +12,48 @@ colors.setTheme({
   info: 'green',
   data: 'grey',
   help: 'cyan',
-  warn: 'yellow',
+  warn: ['yellow', 'bgWhite'],
   debug: 'blue',
-  error: 'red'
+  error: 'red',
+  achivement: 'rainbow'
 });
-//obteniendo configuraciones
-var config = require("./config/config");
-
-//console.log(`> contenido del modulo= ${modulo}`)
+// Obteniendo configruaciones
+var config = require('./config/config');
 var IP = config.IP;
 var PORT = config.PORT;
 // Creando el server
 var server = http.createServer(function(req,res){
-    res.writeHead(
-        200,{
-            'Content-Type':'text/plain',
-            'server':'Buho@0.0.0'
-        }
-    );
-
-res.write('Hola desde el server...');
-res.end();
+    var path = req.url;
+    console.log(`> URL solicitada: ${path}`.silly);
+    if(path == '/index' || path == '/index.html'){
+        path = `static/index.html`;
+        fs.readFile(path, 'utf8', function(err, content){
+            if(err){
+                throw err;
+            }
+            res.writeHead(
+                200,
+                {
+                    'Content-Type': 'text/html',
+                    'Server': 'Buho@0.0.2'
+                }
+            );
+            res.end(content);
+        });
+    } else {
+        res.writeHead(
+            200,
+            {
+                'Content-Type': 'text/html',
+                'Server': 'Buho@0.0.2'
+            }
+        );
+        res.end('<marquee><h1 style="color: orange">EN CONSTRUCCION!!!</h1></marquee>');
+    }
 });
-// Poniendo a escuchar al server
-server.listen(PORT, IP, function(){ 
-    console.log("> server escuchando en ".info+`http://${IP}:${PORT}/ ...`.info);
+// Poniendo a escuchar
+// al server
+server.listen(PORT,IP,function(){
+    console.log("> Server escuchando en ".info +
+    `http://${IP}:${PORT}/ ...`.info);
 });
